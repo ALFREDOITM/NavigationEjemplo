ccbb5f66e70a73378158226663715e1f19ad14ae
ALFREDOITM <alfredosanchez.as68@gmail.com>
2022-03-22 22:27:04 -0600

    Lo de navigation

---
+++ app/src/main/java/com/example/navigationejemplo/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.navigationejemplo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.navigationejemplo.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import java.security.AccessController
+
+class MainActivity : AppCompatActivity() {
+    //private lateinit var navController: NavController
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        binding.bottomNavigationView.setupWithNavController(navHostFragment.navController)
+
+        /*
+        val navView: BottomNavigationView = findViewById(R.id.bottomNavigationView)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        navView.setupWithNavController(navController)
+         */
+    }
+}
